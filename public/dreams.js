c1b30aa4f97cc8686e6b70a0d5f21d6072fffc98
akashkatir <130875454+akashkatir@users.noreply.github.com>
2024-02-19 15:13:01 -0800

    Added dream interpretation button

--- public/dreams.js
+++ public/dreams.js
@@ -21,10 +21,13 @@ querySnapshot.forEach((doc) => {
     const content = document.createElement('div');
     content.textContent = data.dream; 
 
+    const button = document.createElement('button');
+    button.textContent = "Interpret"
+    button.addEventListener("click", openInterpretationPopup)
     // Append the title and content to the new div
     newDiv.appendChild(title);
     newDiv.appendChild(content);
-
+    newDiv.appendChild(button)
     // Append the new div to the target div
     dream_container.appendChild(newDiv);
 
@@ -38,3 +41,30 @@ function formatDate(date){
     const formattedDate = `${day.toString().padStart(2, '0')}-${month.toString().padStart(2, '0')}-${year}`;
     return formattedDate;
 }
+
+function openInterpretationPopup(){
+  document.getElementById("interpretationPopup").style.display = "block";
+  document.getElementById("interpretationPopupText").textContent = "loading"
+
+  var myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/json");
+
+  var raw = JSON.stringify({
+    "dream": "I woke up in a basement surrounded by rats"
+  });
+
+  var requestOptions = {
+    method: 'POST',
+    headers: myHeaders,
+    body: raw,
+    redirect: 'follow'  };
+
+  fetch("https://dreamjournalnode-6dhtfkqjha-uc.a.run.app/interpret", requestOptions)
+    .then(response => response.text())
+    .then(response => document.getElementById("interpretationPopupText").innerHTML = boldText(response))
+    .catch(error => console.log('error', error));
+  }
+
+  function boldText(text) {
+    return text.replace(/\*\*(.*?)\*\*/g, "<strong>$1</strong>");
+  }
